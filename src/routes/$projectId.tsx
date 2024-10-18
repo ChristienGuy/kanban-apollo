f0955272ecdd64e4d5b5a3b9553d953b0528e6d9
Chris Guy <christien.guy@gmail.com>
2024-10-18 15:30:22 +0100

    feat: add ui drag and drop

--- src/routes/$projectId.tsx
+++ src/routes/$projectId.tsx
@@ -1,7 +1,16 @@
 import { useQuery } from "@apollo/client";
 import { createFileRoute } from "@tanstack/react-router";
+import {
+  DragDropContext,
+  Draggable,
+  Droppable,
+  DropResult,
+} from "@hello-pangea/dnd";
+
 import { GET_PROJECT } from "../queries";
 import { GetProjectQuery } from "../__generated__/graphql";
+import { Card, CardHeader } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 
 export const Route = createFileRoute("/$projectId")({
   component: ProjectComponent,
@@ -9,8 +18,10 @@ export const Route = createFileRoute("/$projectId")({
 
 function Column({
   column,
+  index,
 }: {
   column: GetProjectQuery["project"]["columns"][number];
+  index: number;
 }) {
   const sortedTasks = [...column.tasks];
   sortedTasks.sort((a, b) => {
@@ -24,19 +35,62 @@ function Column({
   });
 
   return (
-    <li key={column.id} className="bg-gray-200 p-4 rounded-lg">
-      <ul className="flex flex-col gap-2">
-        <li className="font-bold">{column.title}</li>
-        {sortedTasks.map((task) => (
+    <Draggable draggableId={column.id} index={index}>
+      {(colDraggableProvided) => {
+        return (
           <li
-            className="border shadow-sm bg-white rounded-lg p-2"
-            key={task.id}
+            {...colDraggableProvided.draggableProps}
+            {...colDraggableProvided.dragHandleProps}
+            ref={colDraggableProvided.innerRef}
+            className="bg-gray-200 p-4 rounded-lg"
           >
-            {task.title}
+            <Droppable droppableId={column.id} type="TASK">
+              {(colDroppableProvided) => {
+                return (
+                  <ul
+                    className="flex flex-col"
+                    {...colDroppableProvided.droppableProps}
+                    ref={colDroppableProvided.innerRef}
+                  >
+                    <li className="font-bold">{column.title}</li>
+                    {sortedTasks.map((task, index) => (
+                      <Draggable
+                        key={task.id}
+                        draggableId={task.id}
+                        index={index}
+                      >
+                        {(taskDraggableProvided, snapshot) => {
+                          return (
+                            <li
+                              {...taskDraggableProvided.draggableProps}
+                              {...taskDraggableProvided.dragHandleProps}
+                              key={task.id}
+                              ref={taskDraggableProvided.innerRef}
+                              className="mb-2"
+                            >
+                              <Card
+                                className={cn("transition", {
+                                  "shadow-lg scale-105": snapshot.isDragging,
+                                  "!rotate-0 shadow-sm scale-100":
+                                    snapshot.isDropAnimating,
+                                })}
+                              >
+                                <CardHeader>{task.title}</CardHeader>
+                              </Card>
+                            </li>
+                          );
+                        }}
+                      </Draggable>
+                    ))}
+                    {colDroppableProvided.placeholder}
+                  </ul>
+                );
+              }}
+            </Droppable>
           </li>
-        ))}
-      </ul>
-    </li>
+        );
+      }}
+    </Draggable>
   );
 }
 
@@ -56,16 +110,33 @@ function ProjectComponent() {
     return <div>Error: {error.message}</div>;
   }
 
+  const handleDragEnd = (result: DropResult) => {
+    console.log("drag end", result);
+  };
+
   return (
     <div>
       {data ? (
         <>
           <h1>Project: {data.project.title}</h1>
-          <ul className="grid grid-cols-3 gap-4">
-            {data.project.columns.map((column) => (
-              <Column key={column.id} column={column} />
-            ))}
-          </ul>
+          <DragDropContext onDragEnd={handleDragEnd}>
+            <Droppable droppableId="board" direction="horizontal">
+              {(provided) => {
+                return (
+                  <ul
+                    ref={provided.innerRef}
+                    {...provided.droppableProps}
+                    className="grid grid-cols-3 gap-4"
+                  >
+                    {data.project.columns.map((column, index) => (
+                      <Column key={column.id} column={column} index={index} />
+                    ))}
+                    {provided.placeholder}
+                  </ul>
+                );
+              }}
+            </Droppable>
+          </DragDropContext>
         </>
       ) : (
         <div>No data</div>
--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "tailwindcss";
 import react from "@vitejs/plugin-react-swc";
 import codegen from "vite-plugin-graphql-codegen";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,6 +11,12 @@ export default defineConfig({
   css: {
     postcss: {
       plugins: [tailwindcss()],
+    },
+  },
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
